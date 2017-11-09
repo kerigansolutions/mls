51747606d25713eb109800b122460e82ce290746
Daron Adkins <daron.adkins@gmail.com>
2017-11-09 11:15:24 -0600

    Initial commit

---
+++ src/Communities.php
@@ -0,0 +1,67 @@
+<?php
+namespace KeriganSolutions\MLS;
+
+use Includes\Modules\CPT\CustomPostType;
+
+class Communities
+{
+    /**
+     * @return null
+     */
+    public function createPostType()
+    {
+        $communities = new CustomPostType(
+            'Community',
+            [
+                'supports'           => ['title', 'editor', 'thumbnail', 'revisions'],
+                'menu_icon'          => 'dashicons-location',
+                'has_archive'        => true,
+                'menu_position'      => null,
+                'public'             => true,
+                'publicly_queryable' => true,
+                //'capability_type'    => array('community','communities'),
+            ]
+        );
+
+        $communities->addMetaBox(
+            'Community Info',
+            [
+                'Area Image'    => 'image',
+                'Database Name' => 'text',
+                'Latitude'      => 'text',
+                'Longitude'     => 'text'
+            ]
+        );
+    }
+
+    /*
+     * @return $communities
+     */
+    public function getCommunities()
+    {
+        $communitylist = get_posts([
+            'posts_per_page' => -1,
+            'offset'         => 0,
+            'order'          => 'ASC',
+            'orderby'        => 'menu_order',
+            'post_type'      => 'community',
+            'post_status'    => 'publish',
+        ]);
+
+        $communities = [];
+
+        foreach ($communitylist as $community) {
+            $communities[] = [
+                'id'          => $community->ID,
+                'title'       => $community->post_title,
+                'name'        => get_post_meta($community->ID, 'community_info_database_name', true),
+                'photo'       => get_post_meta($community->ID, 'community_info_area_image', true),
+                'latitude'    => get_post_meta($community->ID, 'community_info_latitude', true),
+                'longitude'   => get_post_meta($community->ID, 'community_info_longitude', true),
+                'link'        => get_permalink($community->ID)
+            ];
+        }
+
+        return $communities;
+    }
+}
---
+++ src/FeaturedListings.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace KeriganSolutions\MLS;
+
+use Includes\Modules\CPT\CustomPostType;
+
+class FeaturedListings
+{
+    public function createPostType()
+    {
+        $listing = new CustomPostType(
+            'Featured Listing',
+            [
+                'supports'           => ['title'],
+                'menu_icon'          => 'dashicons-welcome-add-page',
+                'has_archive'        => false,
+                'menu_position'      => null,
+                'public'             => false,
+                'publicly_queryable' => false,
+                'hierarchical'       => false,
+                'show_ui'            => true,
+                'show_in_nav_menus'  => true,
+                '_builtin'           => false,
+            ]
+        );
+    }
+
+    public function createAdminColumns()
+    {
+        add_filter('manage_featured_listing_posts_columns', function () {
+            $defaults = [
+                'title' => 'MLS Number',
+                'date'  => 'Date Posted'
+            ];
+            return $defaults;
+        }, 0);
+
+        add_filter('enter_title_here', function ($title) {
+            $screen = get_current_screen();
+
+            if ('featured_listing' == $screen->post_type) {
+                $title = 'MLS Number';
+            }
+
+            return $title;
+        });
+    }
+
+    public function getFeaturedListings($args = [])
+    {
+        $request = [
+            'post_type'      => 'featured_listing',
+            'posts_per_page' => -1,
+            'orderby'        => 'menu_order',
+            'order'          => 'ASC',
+            'offset'         => 0,
+            'post_status'    => 'publish',
+        ];
+
+        $request = get_posts(array_merge($request, $args));
+
+        $featuredArray = [];
+        foreach ($request as $item) {
+            $mlsNumber = (isset($item->post_title) ? $item->post_title : null);
+            array_push($featuredArray, $mlsNumber);
+        }
+
+        return $featuredArray;
+    }
+}
